d60abb89141e5ddbcdfcd7e6a1486d209183d14b
Markus Felten <Markus.Felten@gmx.de>
2021-07-29 18:49:06 +0200

    chore: initial checkin

---
+++ src/ava.mjs
@@ -0,0 +1,22 @@
+
+
+
+export default async function test(name, body) {
+
+    let assertions = [];
+
+    const t = {
+        true(value) {
+            assertions.push({});
+            if (value !== true) console.log(`${name}: ${value}`)
+        },
+        false(value) {
+            assertions.push({});
+            if (value !== false) console.log(`${name}: ${value}`)
+        }
+    };
+
+    await body(t);
+
+    console.log(assertions);
+}
