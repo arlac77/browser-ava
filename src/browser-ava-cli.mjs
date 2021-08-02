f10dacf5421d65595bd72dc0d2d282afd9bf38ed
Markus Felten <Markus.Felten@gmx.de>
2021-08-02 23:44:28 +0200

    fest: start web server

--- src/browser-ava-cli.mjs
+++ src/browser-ava-cli.mjs
@@ -1,13 +1,35 @@
 #!/usr/bin/node
 
 import { chromium, firefox, webkit } from 'playwright';
+import { DevServer } from "@web/dev-server-core";
+
+let port = 8080;
+
+const server = new DevServer(
+    {
+      port ,
+      rootDir: process.cwd(),
+      plugins: [],
+      middleware: [],
+    },
+    {
+      log: console.log,
+      debug: console.debug,
+      error: console.error,
+      warn: console.warn,
+      logSyntaxError(error) {
+        console.error(error.message);
+      },
+    },
+  );
+  server.start();
 
 async function run () {
     const browser = await chromium.launch({ headless: false});
 
     console.log(browser);
     const page = await browser.newPage();
-    const x = await page.goto('http://github.com/');
+    const x = await page.goto(`http://localhost:${port}/tests/index.html`);
     console.log(x);
  //   await browser.close();
 }
