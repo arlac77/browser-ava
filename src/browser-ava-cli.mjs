c320fbaf1c7dc258f53bfeb31fe52249d010f282
Markus Felten <Markus.Felten@gmx.de>
2021-08-04 18:58:21 +0200

    test: simple cli

--- src/browser-ava-cli.mjs
+++ src/browser-ava-cli.mjs
@@ -4,34 +4,36 @@ import { chromium, firefox, webkit } from 'playwright';
 import { DevServer } from "@web/dev-server-core";
 
 let port = 8080;
+let headless = true;
 
 const server = new DevServer(
-    {
-      port ,
-      rootDir: process.cwd(),
-      plugins: [],
-      middleware: [],
+  {
+    port,
+    rootDir: process.cwd(),
+    plugins: [],
+    middleware: [],
+  },
+  {
+    log: console.log,
+    debug: console.debug,
+    error: console.error,
+    warn: console.warn,
+    logSyntaxError(error) {
+      console.error(error.message);
     },
-    {
-      log: console.log,
-      debug: console.debug,
-      error: console.error,
-      warn: console.warn,
-      logSyntaxError(error) {
-        console.error(error.message);
-      },
-    },
-  );
-  server.start();
+  },
+);
 
-async function run () {
-    const browser = await chromium.launch({ headless: false});
+server.start();
 
-    console.log(browser);
-    const page = await browser.newPage();
-    const x = await page.goto(`http://localhost:${port}/tests/index.html`);
-    console.log(x);
- //   await browser.close();
+async function run() {
+  const browser = await chromium.launch({ headless });
+
+  console.log(browser);
+  const page = await browser.newPage();
+  const x = await page.goto(`http://localhost:${port}/tests/index.html`);
+  console.log(x);
+  //   await browser.close();
 }
 
 
---
+++ tests/cli-test.mjs
@@ -0,0 +1,17 @@
+import test from "ava";
+import execa from "execa";
+
+test("cli simple", async t => {
+  const p = await execa(
+    "node",
+    [
+      new URL("../src/browser-ava-cli.mjs", import.meta.url).pathname,
+    ],
+    { all: true }
+  );
+
+  t.log(p.all);
+  console.log(p.all);
+
+  t.is(p.exitCode, 0);
+});
