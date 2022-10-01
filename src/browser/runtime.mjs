4186437821ad8b732053ff75f653fb465e796ef4
Markus Felten <Markus.Felten@gmx.de>
2022-10-01 10:48:04 +0200

    refactor: rendering

--- src/browser/runtime.mjs
+++ src/browser/runtime.mjs
@@ -16,30 +16,27 @@ async function displayTests() {
     displayTests();
   };
 
-  const tests = document.getElementById("tests");
-
-  function slot(f, def) {
-    return (
-      `<li id="${f}">${f}<ul>` +
-      def.tests
-        .map(
-          t =>
-            `<li class="${
-              t.passed === true ? "passed" : t.passed === false ? "failed" : ""
-            }">${t.name} <span>${t.assertions
-              .filter(a => !a.passed)
-              .map(a => a.name + " " + a.message)
-              .join(" ")}</span></li>`
-        )
-        .join("\n") +
-      "</ul></li>"
-    );
+  function renderTest(t) {
+    return `<li class="${
+      t.passed === true ? "passed" : t.passed === false ? "failed" : ""
+    }">${t.name} <span>${t.assertions
+      .filter(a => !a.passed)
+      .map(a => a.name + " " + a.message)
+      .join(" ")}</span></li>`;
   }
+
+  function renderFile(f, def) {
+    return `<li id="${f}">${f}<ul>${def.tests
+      .map(renderTest)
+      .join("\n")}</ul></li>`;
+  }
+
+  const tests = document.getElementById("tests");
 
   tests.innerHTML =
     "<ul>" +
     Object.entries(world.files)
-      .map(([f, def]) => slot(f, def))
+      .map(([f, def]) => renderFile(f, def))
       .join("\n") +
     "</ul>";
 }
@@ -73,7 +70,11 @@ function testContext(def) {
       assertions.push({ passed: a === b, message, name });
     },
     is(a, b, name) {
-      assertions.push({ passed: Object.is(a, b), message: `${a} != ${b}`, name });
+      assertions.push({
+        passed: Object.is(a, b),
+        message: `${a} != ${b}`,
+        name
+      });
     },
     true(value, name) {
       assertions.push({
