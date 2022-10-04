ba14102dc46a7b13a1347fc12755720c76ddf1a9
Markus Felten <Markus.Felten@gmx.de>
2022-10-04 20:21:45 +0200

    test: cli failures + passed

--- tests/cli-test.mjs
+++ tests/cli-test.mjs
@@ -1,11 +1,12 @@
 import test from "ava";
 import { execa } from "execa";
 
-test("cli simple", async t => {
+test("cli all passed", async t => {
   const p = await execa(
     "node",
     [
       new URL("../src/browser-ava-cli.mjs", import.meta.url).pathname,
+      "--port", 8091,
       "--no-keep-open",
       "--headless",
       "--chromium",
@@ -13,8 +14,27 @@ test("cli simple", async t => {
     ],
     { all: true }
   );
-
-  t.log(p.all);
-
   t.is(p.exitCode, 0);
+});
+
+test("cli mixed result", async t => {
+  try {
+    const p = await execa(
+      "node",
+      [
+        new URL("../src/browser-ava-cli.mjs", import.meta.url).pathname,
+        "--port", 8092,
+        "--no-keep-open",
+        "--headless",
+        "--chromium",
+        "tests/fixtures/tests/first-test.mjs"
+      ],
+      { all: true }
+    );
+  } catch (p) {
+    t.is(p.exitCode, 1);
+    t.regex(p.all, /6 tests failed/);
+    t.regex(p.all, /1 known failure/);
+    t.regex(p.all, /2 tests todo/);
+  }
 });
