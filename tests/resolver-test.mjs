3607ec4e2f699bd80150b23932cc12b54c6571e0
Markus Felten <Markus.Felten@gmx.de>
2022-10-06 18:43:17 +0200

    tes: more resolver cases

--- tests/resolver-test.mjs
+++ tests/resolver-test.mjs
@@ -9,9 +9,9 @@ rt.title = (title = "resolve", name, pkg, result) =>
   `${title} ${name} => ${result}`;
 
 test(rt, "a", { name: "a", main: "index.mjs" }, "index.mjs");
-test(
-  rt,
-  "a",
-  { name: "a", exports: { ".": "./src/index.mjs" } },
-  "src/index.mjs"
-);
+test(rt, "a", { name: "a", exports: { ".": "./src/a.mjs" } }, "src/a.mjs");
+test(rt, "b", { name: "b", exports: { browser: "./src/b.mjs" } }, "src/b.mjs");
+test(rt, "d", { name: "d", exports: { default: "./src/d.mjs" } }, "src/d.mjs");
+test(rt, "e", { name: "e", exports: "./src/e.mjs" }, "src/e.mjs");
+test(rt, "e/s", { name: "e", exports: "./src/e.mjs" }, undefined);
+test(rt, "e/s", { name: "e", exports: { "./s" : "s.mjs"} }, "s.mjs");
