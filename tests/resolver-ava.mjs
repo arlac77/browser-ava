b1ed5ada25ee8fc34711bb08d760acd311d81757
Markus Felten <Markus.Felten@gmx.de>
2024-03-28 01:18:04 +0100

    test: multiple @ sub modules

--- tests/resolver-ava.mjs
+++ tests/resolver-ava.mjs
@@ -16,6 +16,9 @@ ret.title = (title = "resolveImport", base, name, result) =>
 
 test(ret, "./fixtures", "bar", "/src/bar.mjs");
 test(ret, "./fixtures", "@a/b", "/node_modules/@a/b/ab.mjs");
+test(ret, "./fixtures/node_modules/@a/b", "@a/b", "/ab.mjs");
+test(ret, "./fixtures", "@a/c", "/node_modules/@a/c/ac.mjs");
+test.skip(ret, "./fixtures/node_modules/@a/b", "@a/c", "../c/ac.mjs");
 
 function rest(t, module, pkg, result) {
   t.is(resolveExports(module.split(/\//), pkg), result);
