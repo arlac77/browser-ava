187a00c52aaefc888a8ec081a93e085e8b76635f
Markus Felten <Markus.Felten@gmx.de>
2025-10-04 14:03:58 +0100

    fix: handle deep recursion in isEqual()

--- src/browser/assertions.mjs
+++ src/browser/assertions.mjs
@@ -1,5 +1,12 @@
 export function isEqual(a, b) {
+  return _isEqual(a, b, new Set());
+}
+
+function _isEqual(a, b, seen) {
   if (a !== undefined && b === undefined) {
+    return false;
+  }
+  if (a === undefined && b !== undefined) {
     return false;
   }
 
@@ -7,10 +14,16 @@ export function isEqual(a, b) {
     return Object.is(a, b);
   }
 
+  if(seen.has(a)) {
+    return false;
+  }
+
+  seen.add(a);
+  
   if (Array.isArray(a)) {
     if (a.length === b.length) {
       for (let i = 0; i < a.length; i++) {
-        if (!isEqual(a[i], b[i])) {
+        if (!_isEqual(a[i], b[i], seen)) {
           return false;
         }
       }
@@ -21,10 +34,9 @@ export function isEqual(a, b) {
   }
 
   if (typeof a === "object") {
-
-    if(a instanceof ArrayBuffer) {
-      if(b instanceof ArrayBuffer) {
-         return a.byteLength === b.byteLength;
+    if (a instanceof ArrayBuffer) {
+      if (b instanceof ArrayBuffer) {
+        return a.byteLength === b.byteLength;
       }
     }
 
@@ -32,7 +44,7 @@ export function isEqual(a, b) {
       return (
         b instanceof Set &&
         a.size === b.size &&
-        [...a].every((value) => b.has(value))
+        [...a].every(value => b.has(value))
       );
     }
     if (a instanceof Map) {
@@ -40,7 +52,7 @@ export function isEqual(a, b) {
         return false;
       }
       for (const [k, v] of a.entries()) {
-        if (!isEqual(v, b.get(k))) {
+        if (!_isEqual(v, b.get(k), seen)) {
           return false;
         }
       }
@@ -49,7 +61,7 @@ export function isEqual(a, b) {
     }
 
     for (const key of new Set(Object.keys(a).concat(Object.keys(b)))) {
-      if (!isEqual(a[key], b[key])) {
+      if (!_isEqual(a[key], b[key], seen)) {
         return false;
       }
     }
@@ -66,7 +78,7 @@ const scalarTypes = new Set([
   "string",
   "number",
   "bigint",
-  "boolean",
+  "boolean"
 ]);
 
 export function isScalar(a) {
@@ -75,6 +87,7 @@ export function isScalar(a) {
     a instanceof String ||
     a instanceof Number ||
     a instanceof Function ||
+    a instanceof BigInt ||
     a === null
   );
 }
